405bd779b5688d6bafef5f3128e17a4e4773f10a
Denis Zatsepin <denis@zatsepin.spb.ru>
2014-12-29 11:01:34 +0300

    Add realizing routines.

--- test/entity.test.js
+++ test/entity.test.js
@@ -1,7 +1,7 @@
 var chai      = require('chai');
 var sinon     = require('sinon');
 var sinonChai = require('sinon-chai');
-var should    = chai.should();
+chai.should();
 var expect    = chai.expect;
 var EventEmitter2 = require('eventemitter2').EventEmitter2;
 var Entity    = require('../lib/entity');
@@ -11,6 +11,14 @@ chai.use(sinonChai);
 describe('Entity tests:', function() {
   describe('Entity', function() {
     var entity;
+    var Component = function() {
+      this._entity = null;
+    };
+    Component.prototype.getEntity = function() {
+      return this._entity;
+    };
+    Component.prototype.update = function() {};
+
     before(function() {
       entity = new Entity();
     });
@@ -106,6 +114,73 @@ describe('Entity tests:', function() {
     it('should have componens array', function() {
       ('_components' in entity).should.be.equal(true);
       (entity._components instanceof Array).should.be.equal(true);
+    });
+
+    it('should add new components', function() {
+      var component = new Component();
+      entity.addComponent(component);
+      entity._components.length.should.be.above(0);
+    });
+
+    it('should not add component that has already been added to other entity', function() {
+      var component = new Component();
+      var otherEntity = new Entity();
+      otherEntity.addComponent(component);
+
+      var func = entity.addComponent.bind(entity, component);
+      expect(func).to.throw(Error);
+    });
+
+    it('should reject adding one component twice', function() {
+      var component = new Component();
+      entity.addComponent(component);
+
+      var func = entity.addComponent.bind(entity, component);
+      expect(func).to.throw(Error);
+    });
+
+    it('should remove one component', function() {
+      var component = new Component();
+      entity.addComponent(component);
+      entity._components.indexOf(component).should.be.at.least(0);
+      entity.removeComponent(component);
+      entity._components.indexOf(component).should.be.below(0);
+    });
+
+    it('removed component should have _entity equal to null', function() {
+      var component = new Component();
+      entity.addComponent(component);
+      entity._components.indexOf(component).should.be.at.least(0);
+      entity.removeComponent(component);
+      expect(component._entity).to.equal(null);
+    });
+
+    it('should clear components', function() {
+      var component = new Component();
+      entity.addComponent(component);
+      entity._components.indexOf(component).should.be.at.least(0);
+      entity.clearComponents();
+      expect(component._entity).to.equal(null);
+      entity._components.length.should.be.equal(0);
+    });
+
+    it('should update all components', function() {
+      sinon.spy(Component.prototype, 'update');
+      (function() {
+        for (var i = 0; i < 5; i += 1) {
+          var component = new Component();
+          entity.addComponent(component);
+        }
+        var child = new Entity();
+        entity.addChild(child);
+        for (i = 0; i < 5; i += 1) {
+          component = new Component();
+          child.addComponent(component);
+        }
+      })();
+      entity.update();
+      Component.prototype.update.should.have.callCount(10);
+      Component.prototype.update.restore();
     });
   });
 });
